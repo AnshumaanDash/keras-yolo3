56bc2e9312dcabfbad7ac39df3a79040296d5597
AnshumaanDash <anshumaan.dash@gmail.com>
2019-03-27 21:09:33 +0530

    mAP calculation added

--- validate.py
+++ validate.py
@@ -8,7 +8,7 @@ from yolo3.model import preprocess_true_boxes
 from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 from tqdm import tqdm_notebook as tqdm
 
-class Validate()
+class Validate():
 
     def __init__(self):
 
