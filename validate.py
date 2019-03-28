71e4b44eeb24bd730ff7183f7be77d85f014e2bd
AnshumaanDash <anshumaan.dash@gmail.com>
2019-03-29 02:15:11 +0530

    modifications

--- validate.py
+++ validate.py
@@ -302,8 +302,8 @@ class Validate():
             if len(box)>0:
                 np.random.shuffle(box)
             if len(box)>max_boxes: box = box[:max_boxes]
-            box[:, [0,2]] = box[:, [0,2]]*scale
-            box[:, [1,3]] = box[:, [1,3]]*scale
+            box[:, [0,2]] = box[:, [0,2]]*scale + dx
+            box[:, [1,3]] = box[:, [1,3]]*scale + dy
             box_data[:len(box)] = box
 
             return image_data, box_data
